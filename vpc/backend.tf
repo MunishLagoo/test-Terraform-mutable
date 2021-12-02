2c221fb138f9667887a3952f8ba7272aaf0cd1b7
Munish Lagoo <munishlagoo@gmail.com>
2021-12-02 15:16:11 -0500

    add role to ec2

--- vpc/backend.tf
+++ vpc/backend.tf
@@ -1,10 +1,6 @@
-//terraform {
-//    backend "s3" {
- //       bucket= "test-terraform-us-east-1"
-  //      key= "test-Terraform-mutable/vpc/dev/terraform.tfstate"
-   //     region= "us-east-1"
-   // }
-//}
+terraform {
+    backend "s3" {}  
+}
 
 provider "aws" {
     region= "us-east-1"

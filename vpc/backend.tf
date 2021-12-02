7b29d10c6e89cc4779fdd172d903a222621ee017
Munish Lagoo <munishlagoo@gmail.com>
2021-12-02 14:16:56 -0500

    add

--- vpc/backend.tf
+++ vpc/backend.tf
@@ -1,10 +1,10 @@
-terraform {
-    backend "s3" {
-        bucket= "test-terraform-us-east-1"
-        key= "test-Terraform-mutable/vpc/dev/terraform.tfstate"
-        region= "us-east-1"
-    }
-}
+//terraform {
+//    backend "s3" {
+ //       bucket= "test-terraform-us-east-1"
+  //      key= "test-Terraform-mutable/vpc/dev/terraform.tfstate"
+   //     region= "us-east-1"
+   // }
+//}
 
 provider "aws" {
     region= "us-east-1"

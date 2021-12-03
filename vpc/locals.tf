81b352f7e9a110b62f1b1c85eb89636c7e2277b1
Munish Lagoo <munishlagoo@gmail.com>
2021-12-03 09:57:31 -0500

    commented datafile

--- vpc/locals.tf
+++ vpc/locals.tf
@@ -1,16 +1,16 @@
 //loop in a loop 
 //2 Route Tables and 2 CIDR that makes 4 Combinations
 //each route table should have both CIDR
-# locals{
-# association-list = flatten([
-#     for route_table in tolist(data.aws_route_tables.default-vpc-routes.ids):[
-#         for cidr in local.ALL_VPC_CIDR: {
-#             route_table = route_table
-#             cidr = cidr
-#         }
-#     ]
-# ])
-# }
+locals{
+association-list = flatten([
+    for route_table in tolist(data.aws_route_tables.default-vpc-routes.ids):[
+        for cidr in local.ALL_VPC_CIDR: {
+            route_table = route_table
+            cidr = cidr
+        }
+    ]
+])
+}
 
 //create a list of all the CIDR, 
 //main is string and ADDON is list
@@ -23,9 +23,9 @@ locals{
 
 
 //*******outputs of these local variables
-# output "sample" {
-#     value = local.association-list
-# }
+output "sample" {
+    value = local.association-list
+}
 
 output "ALL_VPC_CIDR" {
     value = [for s in local.ALL_VPC_CIDR:"CIDR = ${s}"]
--- vpc/routes.tf
+++ vpc/routes.tf
@@ -1,25 +1,31 @@
+//This Resource creates Route table with Routes
 resource "aws_route_table" "private-route" {
   vpc_id = aws_vpc.main.id
 
-  route  {
+  route  = [{
     cidr_block                      = var.VPC_CIDR_DEFAULT
     vpc_peering_connection_id       = aws_vpc_peering_connection.peer.id
-  }
+    "carrier_gateway_id"            = ""
+    "destination_prefix_list_id"    = ""
+    "egress_only_gateway_id"        = ""
+    "gateway_id"                    = ""
+    "instance_id"                   = ""
+    "ipv6_cidr_block"               = ""
+    "local_gateway_id"              = ""
+    "nat_gateway_id"                = ""
+    "network_interface_id"          = ""   
+    "transit_gateway_id"            = ""
+    "vpc_endpoint_id"               = ""
+  }]
 
   tags = {
     Name                            = "private-route"
   }
 }
 
-
-# "carrier_gateway_id"            = ""
-    # "destination_prefix_list_id"    = ""
-    # "egress_only_gateway_id"        = ""
-    # "gateway_id"                    = ""
-    # "instance_id"                   = ""
-    # "ipv6_cidr_block"               = ""
-    # "local_gateway_id"              = ""
-    # "nat_gateway_id"                = ""
-    # "network_interface_id"          = ""   
-    # "transit_gateway_id"            = ""
-    # "vpc_endpoint_id"               = ""
+# resource "aws_route" "route_from_default_vpc" {
+#   route_table_id            = "rtb-4fbb3ac4"
+#   destination_cidr_block    = "10.0.1.0/22"
+#   vpc_peering_connection_id = "pcx-45ff3dc1"
+#   depends_on                = [aws_route_table.testing]
+# }

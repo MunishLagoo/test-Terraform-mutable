3a52dc7707975b06b708002806b102994b6931c0
Munish Lagoo <munishlagoo@gmail.com>
2021-12-03 08:36:39 -0500

    commented datafile

--- vpc/data.tf
+++ vpc/data.tf
@@ -1,8 +1,8 @@
-data "aws_route_tables" "default-vpc-route-table" {
-  vpc_id = var.VPC_ID_DEFAULT
-}
+# data "aws_route_tables" "default-vpc-route-table" {
+#   vpc_id = var.VPC_ID_DEFAULT
+# }
 
-//******output of data
-output "default-vpc-route-table" {
-    value = data.aws_route_tables.default-vpc-route-table
-}
+# //******output of data
+# output "default-vpc-route-table" {
+#     value = data.aws_route_tables.default-vpc-route-table
+# }

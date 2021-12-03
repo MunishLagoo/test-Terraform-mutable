71cd58d37092531cd3967b77db275783c0c08b66
Munish Lagoo <munishlagoo@gmail.com>
2021-12-03 15:31:50 -0500

    created Public and Private Subnets addon

---
+++ vpc/ngw.tf
@@ -0,0 +1,18 @@
+//NAT gateway needs elastic Ip to connect to IGW
+//allocation_id
+resource "aws_eip" "ngw" {
+  vpc      = true
+
+  tags = {
+      Name = "${var.ENV}-ngw-ip"
+  }
+}
+
+resource "aws_nat_gateway" "ngw" {
+  allocation_id = aws_eip.ngw.id
+  subnet_id = var.PRIVATE_SUBNETS.*.id[0]
+
+  tags = {
+    Name = "${var.ENV}-ngw"
+  }
+}

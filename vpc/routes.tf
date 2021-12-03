acca2cce98091a629cb4ecc04ec731b78d744f42
Munish Lagoo <munishlagoo@gmail.com>
2021-12-03 08:47:21 -0500

    commented datafile

--- vpc/routes.tf
+++ vpc/routes.tf
@@ -2,12 +2,23 @@ resource "aws_route_table" "private-route" {
   vpc_id = aws_vpc.main.id
 
   route = [ {
-      cidr_block = var.VPC_ID_DEFAULT
-      vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
+    cidr_block                      = var.VPC_ID_DEFAULT
+    vpc_peering_connection_id       = aws_vpc_peering_connection.peer.id
+    "carrier_gateway_id"            = ""
+    "destination_prefix_list_id"    = ""
+    "egress_only_gateway_id"        = ""
+    "gateway_id"                    = ""
+    "instance_id"                   = ""
+    "ipv6_cidr_block"               = ""
+    "local_gateway_id"              = ""
+    "nat_gateway_id"                = ""
+    "network_interface_id"          = ""   
+    "transit_gateway_id"            = ""
+    "vpc_endpoint_id"               = ""
   }]
 
   tags = {
-    Name = "private-route"
+    Name                            = "private-route"
   }
 }
 

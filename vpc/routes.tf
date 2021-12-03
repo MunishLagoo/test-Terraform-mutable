dbcc2036441e674dfa8276a1c648f6fad5746059
Munish Lagoo <munishlagoo@gmail.com>
2021-12-03 08:42:27 -0500

    commented datafile

--- vpc/routes.tf
+++ vpc/routes.tf
@@ -1,13 +1,13 @@
-resource "aws_route_table" "route" {
+resource "aws_route_table" "private-route" {
   vpc_id = aws_vpc.main.id
 
-  route {
+  route = [ {
       cidr_block = var.VPC_ID_DEFAULT
       vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
-  }
+  }]
 
   tags = {
-    Name = "example"
+    Name = "private-route"
   }
 }
 

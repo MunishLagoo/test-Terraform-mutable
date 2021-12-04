b754adbbfcd1fe8f3d1d5760ce81b56516d6496d
Munish Lagoo <munishlagoo@gmail.com>
2021-12-04 15:33:57 -0500

    added RDS mysql

--- db/locals.tf
+++ db/locals.tf
@@ -1,4 +1,4 @@
 locals{
-DEFAULT_VPC_CIDR = split(",",data.terraform_remote_state.vpc.outputs.DEFAULT_VPC_CIDR)
-ALL_CIDR = concat(local.DEFAULT_VPC_CIDR,data.terraform_remote_state.vpc.outputs.ALL_VPC_CIDR)
+VPC_CIDR_DEFAULT = split(",",data.terraform_remote_state.vpc.outputs.VPC_CIDR_DEFAULT)
+ALL_CIDR = concat(local.VPC_CIDR_DEFAULT,data.terraform_remote_state.vpc.outputs.ALL_VPC_CIDR)
 }

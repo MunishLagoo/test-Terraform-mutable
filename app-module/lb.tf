0017b72f0d3973fed13a2cb8c5f56e0f25adf980
Munish Lagoo <munishlagoo@gmail.com>
2021-12-08 15:51:26 -0500

    corrected public LB listner for app-module

--- app-module/lb.tf
+++ app-module/lb.tf
@@ -13,6 +13,7 @@ resource "aws_lb_target_group" "tg" {
     }
 }
 
+// add instance in target group
 resource "aws_lb_target_group_attachment" "tg-attach" {
 count            = length(local.INSTANCE_IDS)
 target_group_arn = aws_lb_target_group.tg.arn
@@ -20,7 +21,7 @@ target_id        = element(local.INSTANCE_IDS,count.index)
 port             = var.PORT
 }
 
-
+//Private load Balancer rule attched target group
 resource "aws_lb_listener_rule" "private" {
 count = var.IS_PRIVATE_LB ? 1 : 0
 listener_arn = data.terraform_remote_state.alb.outputs.PRIVATE_LISTENER_ARN
@@ -37,13 +38,14 @@ condition {
 }
 
 //public listener for frontend
-# resource "aws_lb_listener" "public_listener" {
-#   load_balancer_arn = data.terraform_remote_state.alb.outputs.PUBLIC_ALB_ARN
-#   port              = "80"
-#   protocol          = "HTTP"
+resource "aws_lb_listener" "public_listener" {
+  count = var.IS_PRIVATE_LB ? 0 : 1
+  load_balancer_arn = data.terraform_remote_state.alb.outputs.PUBLIC_ALB_ARN
+  port              = "80"
+  protocol          = "HTTP"
 
-#   default_action {
-#     type = "forward"
-#     target_group_arn = aws_lb_target_group.tg.arn 
-#   }
-# }
+  default_action {
+    type = "forward"
+    target_group_arn = aws_lb_target_group.tg.arn 
+  }
+}

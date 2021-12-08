e2fd20586e402532b2ed2b9dc36534e21501fd1e
Munish Lagoo <munishlagoo@gmail.com>
2021-12-08 07:13:34 -0500

    corrected app-module ec2.tf

--- app-module/ec2.tf
+++ app-module/ec2.tf
@@ -2,7 +2,8 @@ resource "aws_spot_instance_request" "spot" {
     count = var.SPOT_INSTANCE_COUNT
     ami = data.aws_ami.ami.id
     instance_type = var.INSTANCE_TYPE
-    subnet_id = element(data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNETS_IDS,count.index) 
+    subnet_id = element(data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNETS_ID,count.index) 
+    wait_for_fulfillment = true
     vpc_security_group_ids = [aws_security_group.sg.id]
     tags = { Name = local.tags["Name"]}
 }
@@ -16,6 +17,6 @@ resource "aws_instance" "od" {
     tags = { Name = local.tags["Name"]}
 }
 
-# ouputs "INSTANCE_IDS" {
-#     value = local.INSTANCE_IDS
-# }
+outputs "INSTANCE_IDS" {
+    value = local.INSTANCE_IDS
+}

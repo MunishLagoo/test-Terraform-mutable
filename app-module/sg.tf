4d7091866f840c0f92b75305ed32aa8716f84d5f
Munish Lagoo <munishlagoo@gmail.com>
2021-12-08 08:05:38 -0500

    corrected security-gp-ssh

--- app-module/sg.tf
+++ app-module/sg.tf
@@ -9,7 +9,7 @@ resource "aws_security_group" "sg" {
       from_port        = var.PORT
       to_port          = var.PORT
       protocol         = "tcp"
-      cidr_blocks      = local.ALL_VPC_CIDR
+      cidr_blocks      = local.ALL_CIDR
       ipv6_cidr_blocks = []
       prefix_list_ids  = []
       security_groups  = []

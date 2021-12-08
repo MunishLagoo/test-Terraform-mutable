d3b260abb6225f94f12ab69a4bac767d4362b188
Munish Lagoo <munishlagoo@gmail.com>
2021-12-08 06:34:24 -0500

    corrected main.tf

--- app-module/app-setup.tf
+++ app-module/app-setup.tf
@@ -3,8 +3,8 @@ resource "null_resource" "app-deploy" {
     provisioner "remote-exec" {
         connection {
             host= element(local.PRIVATE_IPS,count.index)
-            user= local.SSH_USER
-            password = local.SSH_PASS
+            user= local.ssh_user
+            password = local.ssh_pass
         }
         inline = [
             "ansible-pull -U https://DevOps-Batches@dev.azure.com/DevOps-Batches/DevOps60/_git/ansible roboshop-pull.yml -e ENV=${var.ENV} -e COMPONENT=${var.COMPONENT} -e APP_VERSION=${var.APP_VERSION} -e NEXUS_USER=${var.NEXUS_USER} -e NEXUS_PASS=${var.NEXUS_PASS}"

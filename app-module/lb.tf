4c89c8ea8b5b7ac5481f4113d0f5dc95a4316ffb
Munish Lagoo <munishlagoo@gmail.com>
2021-12-08 06:36:04 -0500

    corrected main.tf

--- app-module/lb.tf
+++ app-module/lb.tf
@@ -21,7 +21,7 @@ port             = var.PORT
 }
 
 
-resource "aws_lb_listner_rule" "private" {
+resource "aws_lb_listener_rule" "private" {
 count = var.IS_PRIVATE_LB ? 1 : 0
 listner_arn = data.terraform_remote_state.alb.outputs.PRIVATE_LISTENER_ARN
 priority = var.LB_RULE_PRIORITY

542166578fe0b835f98406f4de14ddd4aca4695f
Munish Lagoo <munishlagoo@gmail.com>
2021-12-21 05:39:42 -0500

    attached redis security group

--- db/redis-elasticache.tf
+++ db/redis-elasticache.tf
@@ -6,6 +6,7 @@ resource "aws_elasticache_cluster" "redis" {
   parameter_group_name = aws_elasticache_parameter_group.redis.name
   engine_version       = "6.x"
   port                 = 6379
+  security_group_ids   = [aws_security_group.redis.id]
 }
 
 resource "aws_security_group" "redis" {

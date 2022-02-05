49e67dfca117c036e199097abcca5cdc5e359a5c
Munish Lagoo <munishlagoo@gmail.com>
2022-02-04 21:29:11 -0500

    efs-new access point

--- vpc/efs.tf
+++ vpc/efs.tf
@@ -52,7 +52,7 @@ resource "aws_efs_mount_target" "efs-mt-shared" {
     security_groups = [aws_security_group.ingress-efs.id]
 }
 
-resource "aws_efs_access_point" "efs-access-points" {
+resource "aws_efs_access_point" "efs-cart" {
     file_system_id = aws_efs_file_system.efs-shared.id
     posix_user  {
         uid = 4000
@@ -64,6 +64,125 @@ resource "aws_efs_access_point" "efs-access-points" {
         creation_info  {
             owner_uid = 4000
             owner_gid = 4000
+            permissions = "0755"
+        }
+    }
+}
+
+resource "aws_efs_access_point" "efs-user" {
+    file_system_id = aws_efs_file_system.efs-shared.id
+    posix_user  {
+        uid = 4001
+        gid = 4001
+    }
+
+    root_directory  {
+        path = "/apps/user"
+        creation_info  {
+            owner_uid = 4001
+            owner_gid = 4001
+            permissions = "0755"
+        }
+    }
+}
+
+resource "aws_efs_access_point" "efs-shipping" {
+    file_system_id = aws_efs_file_system.efs-shared.id
+    posix_user  {
+        uid = 4002
+        gid = 4002
+    }
+
+    root_directory  {
+        path = "/apps/shipping"
+        creation_info  {
+            owner_uid = 4002
+            owner_gid = 4002
+            permissions = "0755"
+        }
+    }
+}
+
+resource "aws_efs_access_point" "efs-payment" {
+    file_system_id = aws_efs_file_system.efs-shared.id
+    posix_user  {
+        uid = 4003
+        gid = 4003
+    }
+
+    root_directory  {
+        path = "/apps/payment"
+        creation_info  {
+            owner_uid = 4003
+            owner_gid = 4003
+            permissions = "0755"
+        }
+    }
+}
+
+resource "aws_efs_access_point" "efs-catalogue" {
+    file_system_id = aws_efs_file_system.efs-shared.id
+    posix_user  {
+        uid = 4004
+        gid = 4004
+    }
+
+    root_directory  {
+        path = "/apps/catalogue"
+        creation_info  {
+            owner_uid = 4004
+            owner_gid = 4004
+            permissions = "0755"
+        }
+    }
+}
+
+resource "aws_efs_access_point" "efs-frontend" {
+    file_system_id = aws_efs_file_system.efs-shared.id
+    posix_user  {
+        uid = 4005
+        gid = 4005
+    }
+
+    root_directory  {
+        path = "/apps/frontend"
+        creation_info  {
+            owner_uid = 4005
+            owner_gid = 4005
+            permissions = "0755"
+        }
+    }
+}
+
+resource "aws_efs_access_point" "efs-db-mysql" {
+    file_system_id = aws_efs_file_system.efs-shared.id
+    posix_user  {
+        uid = 5000
+        gid = 5000
+    }
+
+    root_directory  {
+        path = "/db/mysql"
+        creation_info  {
+            owner_uid = 5000
+            owner_gid = 5000
+            permissions = "0755"
+        }
+    }
+}
+
+resource "aws_efs_access_point" "efs-db-mongodb" {
+    file_system_id = aws_efs_file_system.efs-shared.id
+    posix_user  {
+        uid = 5001
+        gid = 5001
+    }
+
+    root_directory  {
+        path = "/db/mongo"
+        creation_info  {
+            owner_uid = 5001
+            owner_gid = 5001
             permissions = "0755"
         }
     }

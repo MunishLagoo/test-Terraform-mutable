1ad1ebb300549fc75f1d5ee11515dc6424eed0e6
Munish Lagoo <munishlagoo@gmail.com>
2022-02-05 18:47:37 -0500

    add eks.tf

--- eks-manage-cluster/vars.tf
+++ eks-manage-cluster/vars.tf
@@ -2,7 +2,7 @@ variable "ENV" {}
 variable "EKS_CLUSTER" {}
 variable "EKS_REGION" {}
 variable "EKS_delete" {
-    type = "string"
+    type = string
     default = "false"
     description = "Used to delete EKS Cluster"
 }

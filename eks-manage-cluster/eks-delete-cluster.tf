227a742ebff1e5a2c27c98f6beba485a48fc489c
Munish Lagoo <munishlagoo@gmail.com>
2022-02-05 18:27:37 -0500

    add eks.tf

---
+++ eks-manage-cluster/eks-delete-cluster.tf
@@ -0,0 +1,6 @@
+resource "null_resource" "eks_delete_cluster" {
+    count = "${var.EKS_delete != "false" ? 1 :0}"
+    provisioner "local-exec" {
+        command = "eksctl delete cluster --name ${var.ENV}-${var.EKS_CLUSTER} --region ${var.EKS_REGION}"
+    }
+}
--- eks-manage-cluster/vars.tf
+++ eks-manage-cluster/vars.tf
@@ -1,3 +1,8 @@
 variable "ENV" {}
 variable "EKS_CLUSTER" {}
 variable "EKS_REGION" {}
+variable "EKS_delete" {
+type = "string"
+default = "false"
+description = "Used to delete EKS Cluster"
+}

7c0305555369cec20d35f493dce444b5415abbec
Munish Lagoo <munishlagoo@gmail.com>
2022-02-05 16:13:39 -0500

    add eks.tf

--- vpc/eks-manage-cluster.tf
+++ vpc/eks-manage-cluster.tf
@@ -1,5 +1,5 @@
 resource "null_resource" "eks_test_vpc" {
     provisioner "local-exec" {
-        command = "eksctl create cluster --name ${var.ENV}-${var.EKS_CLUSTER} --region ${var.EKS_REGION} --managed --vpc-private-subnets=aws_subnet.private-subnet.*.id --vpc-public-subnets=aws_subnet.public-subnet.*.id"
+        command = "eksctl create cluster --name ${var.ENV}-${var.EKS_CLUSTER} --region ${var.EKS_REGION} --managed --vpc-private-subnets=data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNETS_IDS --vpc-public-subnets=data.terraform_remote_state.vpc.outputs.PUBLIC_SUBNETS_IDS"
     }
 }

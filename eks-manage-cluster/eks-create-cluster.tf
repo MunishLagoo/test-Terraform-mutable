e8513ae6362f3a8f2372e51a5d1fbc4e10526ba7
Munish Lagoo <munishlagoo@gmail.com>
2022-02-05 18:45:34 -0500

    add eks.tf

--- eks-manage-cluster/eks-create-cluster.tf
+++ eks-manage-cluster/eks-create-cluster.tf
@@ -1,5 +1,5 @@
 resource "null_resource" "eks_create_cluster" {
-    count = "${var.EKS_delete} = "false" ? 1 :0"
+    count = "${var.EKS_delete} ? 0 : 1"
     provisioner "local-exec" {
         command = "eksctl create cluster --name ${var.ENV}-${var.EKS_CLUSTER} --region ${var.EKS_REGION} --managed --vpc-private-subnets=${local.EKS_PRIVATE_SUBNET} --vpc-public-subnets=${local.EKS_PUBLIC_SUBNET}"
     }
--- eks-manage-cluster/eks-delete-cluster.tf
+++ eks-manage-cluster/eks-delete-cluster.tf
@@ -1,5 +1,5 @@
 resource "null_resource" "eks_delete_cluster" {
-    count = "${var.EKS_delete} != "false" ? 1 :0"
+    count = "${var.EKS_delete} ? 1 : 0"
     provisioner "local-exec" {
         command = "eksctl delete cluster --name ${var.ENV}-${var.EKS_CLUSTER} --region ${var.EKS_REGION}"
     }

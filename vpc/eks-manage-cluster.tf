e96a749804b851c7f62c452b440ca9e67bf31f4a
Munish Lagoo <munishlagoo@gmail.com>
2022-02-05 16:04:12 -0500

    add eks.tf

--- vpc/eks-manage-cluster.tf
+++ vpc/eks-manage-cluster.tf
@@ -1,5 +1,5 @@
 resource "null_resource" "eks_test_vpc" {
     provisioner "local-exec" {
-        command = "eksctl create cluster --name ${var.ENV}-${var.EKS_CLUSTER} --region ${var.EKS_REGION} --managed --vpc-private-subnets=aws_subnet.private-subnet.*.id[0] --vpc-public-subnets=aws_subnet.public-subnet.*.id[0]"
+        command = "eksctl create cluster --name ${var.ENV}-${var.EKS_CLUSTER} --region ${var.EKS_REGION} --managed --vpc-private-subnets=aws_subnet.private-subnet.*.id --vpc-public-subnets=aws_subnet.public-subnet.*.id"
     }
 }

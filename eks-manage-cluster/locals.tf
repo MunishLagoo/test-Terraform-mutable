804e4650b9bb5d124865beac1a633f428363ad58
Munish Lagoo <munishlagoo@gmail.com>
2022-02-05 17:45:48 -0500

    add eks.tf

--- eks-manage-cluster/locals.tf
+++ eks-manage-cluster/locals.tf
@@ -1,4 +1,4 @@
 locals{
-    EKS_PRIVATE_SUBNET = data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNETS_IDS
-    EKS_PUBLIC_SUBNET = data.terraform_remote_state.vpc.outputs.PUBLIC_SUBNETS_IDS
+    EKS_PRIVATE_SUBNET = data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNETS_IDS[0]
+    EKS_PUBLIC_SUBNET = data.terraform_remote_state.vpc.outputs.PUBLIC_SUBNETS_IDS[0]
 }
